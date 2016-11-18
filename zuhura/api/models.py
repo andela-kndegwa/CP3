b95105c0c76d5dec2dc143dd8c0a9c8aabae6423
Kimani Ndegwa <kimani.ndegwa@andela.com>
2016-11-18 12:37:03 +0300

    [Chore #134574517] Configures models appropriatey for the api [Chore #134574517] Runs successful migrations on the api app

--- zuhura/api/models.py
+++ zuhura/api/models.py
@@ -3,3 +3,25 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class BaseModel(models.Model):
+    name = models.CharField(blank=False, max_length=200)
+    description = models.CharField(blank=False, max_length=600)
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('modified_on',)
+        abstract = True
+
+
+class BucketList(BaseModel):
+    owner = models.ForeignKey('auth.User', related_name='bucketlists')
+
+
+class BucketListItem(BaseModel):
+    is_done = models.BooleanField(default=False)
+    bucketlist = models.ForeignKey(
+        'BucketList',
+        related_name='items', on_delete=models.CASCADE,)
