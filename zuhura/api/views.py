c0dc5abf1523e29dffeb19efc67cd9eaa37825f2
Kimani Ndegwa <kimani.ndegwa@andela.com>
2016-11-19 14:44:39 +0300

    (Chore) Ensures Models work between User and Bukcetlists (Chore) Basic minimum of API functionality works (Chore) Refactors serializers.py to allow for proper serialization

--- zuhura/api/views.py
+++ zuhura/api/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework.decorators import api_view
+from rest_framework import permissions
+
+from api.serializers import BucketListSerializer
+from api.models import BucketList
+from api.permissions import IsOwnerOrReadOnly
 # Create your views here.
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'buckelists': reverse('buckelists', request=request, format=format)
+    })
+
+
+class BucketListViewSet(viewsets.ModelViewSet):
+    queryset = BucketList.objects.all()
+    serializer_class = BucketListSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
